35ea1a06abe3aec0a847aac3a9f5804556039ed9
xnhl <mwtheta@protonmail.com>
2022-06-24 18:03:59 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="hello world" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: { HelloWorld }
+}
+</script>
+
+<style lang="sass">
+@font-face
+	font-family: 'Open Sans'
+	src: url('/fonts/OpenSans-Regular.ttf') format('truetype')
+	font-weight: normal
+	font-style: normal
+#app
+	font-family: 'Open Sans', 'Roboto', sans-serif
+	font-size: 14px
+	-ms-text-size-adjust: 100%
+	-webkit-text-size-adjust: 100%
+	-moz-osx-font-smoothing: grayscale
+	-webkit-font-smoothing: antialiased
+	box-sizing: border-box
+*, *:before, *:after
+	box-sizing: border-box
+	font-size: 14px
+	padding: 0
+	margin: 0
+	line-height: 1.1rem
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <p v-text="msg"></p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="sass"></style>
